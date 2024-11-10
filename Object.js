f0022ef9315813ab576845f5020331c8d68443b6
GiovanniTammaro9 <gio_play@live.it>
2024-11-11 00:31:32 +0100

    Object entries

--- Object.js
+++ Object.js
@@ -3,7 +3,16 @@ let student = {
     age: "29",
     grade: "tutor",
     school: "miaaccademy"
-};
+    
+   
+
+    };
+   
+
+
 
 console.log(Object.keys(student));
 console.log(Object.values(student));
+console.log(Object.entries(student));
+
+//non ho capito come stampare in maniera legibile le coppie chiave valore
