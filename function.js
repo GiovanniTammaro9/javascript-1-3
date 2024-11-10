425ca38a940d32b7ef2f34dff1903e47b1dd8567
GiovanniTammaro9 <gio_play@live.it>
2024-11-10 23:34:36 +0100

    utilizzato il codice precedente per persistenza delle variabili

--- function.js
+++ function.js
@@ -1,15 +1,20 @@
-function outerfunciotn(x) {
+function outerfunciotn(x, initialValue) {
+    let result = initialValue;
     
 
     function innerfunction(y) {
-        return x + y;
+        return y + result;
     }
 
     return innerfunction(4);
 }
 
-let risultato = outerfunciotn(5);
+let risultato = outerfunciotn(5, 10);
 console.log(risultato);
+
+//persistenza delle variabili
+
+
 
 
 
